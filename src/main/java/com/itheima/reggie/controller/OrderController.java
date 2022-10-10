84038517bf2ae11bd1d3c7c5c06afd9f513c42fe
will <767572558@qq.com>
2022-10-10 16:05:58 +0800

    订单管理---后台管理订单信息分页查询&&派送订单状态更改

--- src/main/java/com/itheima/reggie/controller/OrderController.java
+++ src/main/java/com/itheima/reggie/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.itheima.reggie.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itheima.reggie.common.BaseContext;
 import com.itheima.reggie.common.R;
@@ -19,6 +20,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Time;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -101,4 +103,58 @@ public class OrderController {
 
         return R.success(ordersDtoPage);
     }
+
+
+    /**
+     * 后台管理订单信息分页查询
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    @GetMapping("/page")
+    @ApiOperation(value = "后台管理订单信息分页查询接口")
+    public R<Page> page(int page, int pageSize, String number, String beginTime, String endTime){
+        //构造分页构造器
+        Page<Orders> pageInfo = new Page(page, pageSize);
+
+        //构造条件构造器
+        LambdaQueryWrapper<Orders> queryWrapper = new LambdaQueryWrapper<>();
+        //添加过滤条件
+        queryWrapper.like(number != null, Orders::getId, number);
+        queryWrapper.ge(beginTime != null, Orders::getOrderTime, beginTime);
+        queryWrapper.le(endTime != null, Orders::getOrderTime, endTime);
+        //添加排序条件
+        queryWrapper.orderByDesc(Orders::getCheckoutTime);
+
+        //执行查询
+        orderService.page(pageInfo, queryWrapper);
+
+        List<Orders> records = pageInfo.getRecords();
+        records = records.stream().map((item) -> {
+            item.setUserName(item.getUserId().toString());
+            return item;
+        }).collect(Collectors.toList());
+
+        return R.success(pageInfo);
+    }
+
+    /**
+     * 派送订单状态更改
+     * @param orders
+     * @return
+     */
+    @PutMapping
+    @ApiOperation(value = "派送订单状态更改接口")
+    public R<String> updateOrder(@RequestBody Orders orders){
+        //构造条件构造器
+        LambdaUpdateWrapper<Orders> updateWrapper = new LambdaUpdateWrapper<>();
+        //添加过滤条件
+        updateWrapper.eq(Orders::getId, orders.getId());
+        updateWrapper.set(Orders::getStatus, orders.getStatus());
+        orderService.update(updateWrapper);
+
+        return R.success("订单派送成功");
+    }
+
+
 }
